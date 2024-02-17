fa98e937b9f66faef070625d33c89a051bc5ba3b
Cameron Bannasch <cameron.bannasch@gmail.com>
2024-02-16 21:59:41 -0800

    Switch between sound effects randomly

--- src/voice/voice.rs
+++ src/voice/voice.rs
@@ -1,10 +1,13 @@
 use poise::serenity_prelude::{VoiceState, UserId};
 use serenity::{async_trait, prelude::{Context, EventHandler}};
+use rand::{seq::SliceRandom, rngs::OsRng};
 
 pub struct Handler;
 
 const GUILD_IDS: &'static [u64] = &[718661117483810816, 923782312087408700];
+// const TARGET_ID: UserId = poise::serenity_prelude::UserId(445421191302217739);
 const TARGET_ID: UserId = poise::serenity_prelude::UserId(398298273699594240);
+const POSSIBLE_FILES: &'static [&str] = &["foghorn.mp3", "tuba.mp3", "nerd.mp3"];
 
 #[async_trait]
 impl EventHandler for Handler {
@@ -29,8 +32,12 @@ impl EventHandler for Handler {
             None => {},
         }
 
+        let mut rng = OsRng::default();
+
+        let chosen_song = POSSIBLE_FILES.choose(&mut rng).expect("Configuration issue, POSSIBLE_FILES should not be empty");
+
         let manager = songbird::get(&ctx).await.expect("Could not get songbird context :skull_emoji:");
-        let song = songbird::ffmpeg("./foghorn.mp3").await.expect("Could not play foghorn.mp3");
+        let song = songbird::ffmpeg(format!("./{}", chosen_song)).await.expect("Could not play foghorn.mp3");
         let handler = manager.join(new.guild_id.expect("This should exist by this point"), new.channel_id.expect("This should exist by this point")).await;
 
         let mut lock = handler.0.lock().await;
